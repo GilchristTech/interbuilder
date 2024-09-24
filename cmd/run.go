ab1df858651900336ea92ece9a7b962d2ce5748b
Gilchrist Pitts <me@gilchrist.tech>
2024-09-23 23:54:54 -0700

    Move CLI run command to cmd/run.go from cmd/main.go

---
+++ cmd/run.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+  . "gilchrist.tech/interbuilder"
+
+  "github.com/spf13/cobra"
+
+  "fmt"
+  "os"
+  "io"
+  "encoding/json"
+)
+
+
+var cmd_run = & cobra.Command {
+  Use: "run [file]",
+  Short: "Run from a build specification file",
+  Args: cobra.ExactArgs(1),
+  Run: func (cmd *cobra.Command, args []string) {
+    var root       *Spec = MakeDefaultRootSpec()
+    var spec_file string = args[0]
+
+    // handle flag: --print-spec
+    //
+    if print_spec {
+      defer func () {
+        fmt.Println()
+        PrintSpec(root)
+      }()
+    }
+
+    // Load spec configuration from file
+    //
+    specs_bytes, err := os.ReadFile(spec_file)
+    if err != nil {
+      fmt.Printf("Could not read spec file: %v\n", err)
+      os.Exit(1)
+    }
+
+    if err := json.Unmarshal(specs_bytes, &root.Props); err != nil {
+      fmt.Printf("Could not parse spec json file: %v\n", err)
+      os.Exit(1)
+    }
+
+    // Create tasks for outputs
+    //
+    for output_i, output_dest := range flag_outputs {
+      var task_name = fmt.Sprintf("cli-output-%d", output_i)
+      var writer io.Writer
+
+      if output_dest == "-" {
+        writer = os.Stdout
+      } else {
+        writer, err = os.Create(output_dest)
+        if err != nil {
+          fmt.Println(err)
+          os.Exit(1)
+        }
+      }
+
+      root.EnqueueTaskMapFunc(task_name, func (a *Asset) (*Asset, error) {
+        asset_json, err := AssetJsonMarshal(a)
+        if err != nil {
+          return nil, err
+        }
+        writer.Write(asset_json)
+        writer.Write([]byte("\n"))
+        return a, nil
+      })
+
+      // Defer a Task to close the file, if applicable
+      //
+      if closer, ok := writer.(io.Closer); ok {
+        if closer == os.Stdout {
+          goto DONT_CLOSE
+        }
+
+        var task_name  = fmt.Sprintf("cli-output-close-%d", output_i)
+        var close_task = root.DeferTaskFunc(task_name, func (*Spec, *Task) error {
+          closer.Close()
+          return nil
+        })
+        close_task.IgnoreAssets = true
+      }
+      DONT_CLOSE:
+    }
+
+    // Resolve
+    //
+    if err = root.Resolve() ; err != nil {
+      fmt.Printf("Error while resolving build specs: %v\n", err)
+      os.Exit(1)
+    }
+
+    // Run tasks
+    //
+    if err = root.Run() ; err != nil {
+      fmt.Printf("Error while running build specs: %v\n", err)
+      os.Exit(1)
+    }
+  },
+}
+
+
